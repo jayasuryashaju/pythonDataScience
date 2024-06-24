32926b1ddb81c4b1ef202ae4cb6cc23239a55dbd
jayasuryashaju <jayasuryashaju3031@gmail.com>
2024-06-24 08:26:22 +0530

    concept of tuple

---
+++ Collections/concept_of_tuple.py
@@ -0,0 +1,16 @@
+"""
+Tuple:
+
+how to define tuple?
+    tu = ()
+    tu = tuple()
+
+properties:
+    supports duplicate.
+    preserves insertion order.
+    tuple in immutable.
+    
+"""
+
+tu = (1, 2, 3, 4, 5, 'tuple', 1.4, 1)
+print(tu)
